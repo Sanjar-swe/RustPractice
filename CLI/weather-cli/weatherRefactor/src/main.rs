69690fa7111dbbb85a5bdce5c60c140f300562c8
Sanjar <sanjarubuntu@gmail.com>
2025-06-22 18:10:07 +0500

    formatted code

--- CLI/weather-cli/weatherRefactor/src/main.rs
+++ CLI/weather-cli/weatherRefactor/src/main.rs
@@ -15,17 +15,15 @@ use crate::output::display_weather_info;
 
 
 fn main() {
-    let args = Args::parse();
+    let args: Args = Args::parse();
     println!("{:?}", args);
 
-    let api_key = dotenv::var("API_KEY").expect("API_KEY must be set");
-    let country_code = &args.country;
-    let response = get_weather_info(&args.city, &country_code, &api_key).unwrap();
+    let api_key: String = dotenv::var("API_KEY").expect("API_KEY must be set");
+    let country_code: &String = &args.country;
+    let response: models::WeatherResponse = get_weather_info(&args.city, &country_code, &api_key).unwrap();
     display_weather_info(&response, &args.format);
-    
-   
 
     println!("{}", "Welcome to Weather Station!".bright_yellow());
-
+    
     
 }
